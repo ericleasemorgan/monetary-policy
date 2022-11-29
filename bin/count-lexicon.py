41d4a88272dd267e2724ee21adc59adc0b4bb627
Eric Lease Morgan <emorgan@nd.edu>
2022-11-29 14:15:05 -0500

    Added stopword elimination

--- bin/count-lexicon.py
+++ bin/count-lexicon.py
@@ -6,22 +6,30 @@
 # (c) University of Notre Dame; distributed under a GNU Public License
 
 # September 29, 2022 - first investigations
+# November  29, 2022 - took stopwords into account; change the value of WINDOW to adjust
 
 
 # configure
-TEXT    = './corpus/txt'
-PATTERN = '*.txt'
-LEXICON = './etc/lexicon.txt'
+TEXT      = './corpus/txt'
+PATTERN   = '*.txt'
+LEXICON   = './etc/lexicon.txt'
+STOPWORDS = './etc/stopwords-lexicon.txt'
+WINDOW    = 80
 
 # require
 from pathlib import Path
 import pandas as pd
 import re
+from nltk.tokenize import word_tokenize
 
 # read and normalize the lexicon
 with open( LEXICON ) as handle : lexicon = handle.readlines()
 lexicon = [ word.strip() for word in lexicon ]
 lexicon = [ word.lower() for word in lexicon ]
+
+# get the stop words
+with open( STOPWORDS ) as handle : stopwords = handle.readlines()
+stopwords = [ word.strip() for word in stopwords ]
 
 # process each file in the given configuration; create a list of rows
 rows = []
@@ -34,7 +42,7 @@ for file in Path( TEXT ).glob( PATTERN ) :
 	
 	# slurp up the file
 	with open( file ) as handle : data = handle.read().lower()
-	
+			
 	# get the size of the file, measured in words (more or less, mostly more)
 	size = len( data.split() )	
 	
@@ -46,8 +54,32 @@ for file in Path( TEXT ).glob( PATTERN ) :
 	
 		# count the number of times the lexicon item is found in the data
 		count = data.count( item )
+
+		# find and process all positions matching the query; finditer does the magic
+		matches = re.finditer( '\\b' + item + '\\b', data )
+		for match in matches :
+	
+			# re-initialize
+			start = match.start()
+			end   = match.end()
 		
-		# divide the count by the size of the given file to create a relative counts
+			# get the characters before and after the item
+			before = data[ start - WINDOW : start ]
+			after  = data[ end            : end + WINDOW ]
+
+			# build a snippet and tokenize the result
+			snippet = before + ' ' + item + ' ' + after
+			words   = word_tokenize( snippet )
+						
+			# process each stop word; check for stopwords
+			for stopword in stopwords:
+			
+				# de-increment and discontinue, conditionally
+				if stopword in words :
+					count = count - 1
+					break
+				
+		# divide the count by the size of the given file to create relative counts
 		weight = count / size
 		
 		# update the row
